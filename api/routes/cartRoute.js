4b6906c4981a0bd90d8c3eef8ece58d1512f5012
Nakamura <nakamura.natalia22@gmail.com>
2021-11-15 15:56:44 -0300

    Creating the Carts routes

---
+++ api/routes/cartRoute.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const CartController = require('../controllers/CartsController');
+const cart = new CartController
+
+const router = Router()
+
+router
+    .post('/api/v1/carts', cart.create)
+    .get('/api/v1/carts/:id', cart.findById)
+    .put('/api/v1/carts/:id', cart.update)
+    .delete('/api/v1/carts/:id', cart.delete)
+
+module.exports = router;
