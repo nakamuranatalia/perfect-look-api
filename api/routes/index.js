2ed78314da64e485149c086c25f2ae659a0069dc
Nakamura <nakamura.natalia22@gmail.com>
2021-11-15 15:58:06 -0300

    Adding cart routes in routes/index.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 const express = require ('express')
 const products = require('./productsRoute')
 const users = require('./usersRoute')
+const cart = require('./cartRoute')
 
 module.exports = app => {
     app.use(express.json(),
     products,
-    users)
+    users,
+    cart)
 }
