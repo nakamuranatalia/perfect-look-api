413ccd1a8ddb26f67a0d29781824708a70fecdc8
Nakamura <nakamura.natalia22@gmail.com>
2021-11-19 18:53:32 -0300

    Creating PurchaseOders migration

---
+++ api/migrations/20211119192245-create-purchase-orders.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PurchaseOrders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {model: 'Users', key:'id'}
+      },
+      totalPrice: {
+        type: Sequelize.FLOAT
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      paymentType: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PurchaseOrders');
+  }
+};
