cc20649976fa50a66bfdc93cd660f25eb85bbf9e
Nakamura <nakamura.natalia22@gmail.com>
2021-11-21 16:52:47 -0300

    Adding PurchaseOrders to routes/index.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,10 +2,12 @@ const express = require ('express')
 const products = require('./productsRoute')
 const users = require('./usersRoute')
 const cart = require('./cartRoute')
+const order = require('./purchaseOrdesRoute')
 
 module.exports = app => {
     app.use(express.json(),
     products,
     users,
-    cart)
+    cart, 
+    order)
 }
