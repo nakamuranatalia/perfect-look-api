6ff07b7e24d6464244f0d33bb42d99bd8431e104
Nakamura <nakamura.natalia22@gmail.com>
2021-11-22 13:02:10 -0300

    Adding recommendationRoute to routes/index.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,11 +3,13 @@ const products = require('./productsRoute')
 const users = require('./usersRoute')
 const cart = require('./cartRoute')
 const order = require('./purchaseOrdesRoute')
+const recommendation = require('./recommendationRoute')
 
 module.exports = app => {
     app.use(express.json(),
     products,
     users,
     cart, 
-    order)
+    order,
+    recommendation)
 }
